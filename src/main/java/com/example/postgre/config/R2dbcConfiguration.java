42b9fbb8e62e34751fa301fd06fb2a162ff2c463
Grigoriy Ryabov <ryabovgs@gmail.com>
2023-02-09 18:56:39 +0300

    Some cleanup.

--- src/main/java/com/example/postgre/config/R2dbcConfiguration.java
+++ src/main/java/com/example/postgre/config/R2dbcConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.postgre.config;
 
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+
 @Configuration
 public class R2dbcConfiguration extends AbstractR2dbcConfiguration {
 
